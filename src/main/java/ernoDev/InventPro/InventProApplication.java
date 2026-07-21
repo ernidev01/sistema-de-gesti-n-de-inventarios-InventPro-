72536b45884f719b54473afdc06f5f33b4ec0021
Erick Nicolas Gonzalez <nicolo288@hotmail.com>
2026-07-20 19:24:28 -0500

    estructura del proyecto

---
+++ src/main/java/ernoDev/InventPro/InventProApplication.java
@@ -0,0 +1,13 @@
+package ernoDev.InventPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ernoDev/InventPro/InventProApplicationTests.java
@@ -0,0 +1,13 @@
+package ernoDev.InventPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
